0455cf0c27d6a30b3eec4b8aad6abc368a78e622
Pavan <pavan10192003@gmail.com>
2025-05-27 15:54:44 +0530

    new5

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {  Routes, Route } from "react-router-dom";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
+import { HashRouter as BrowserRouter } from "react-router-dom";
 
 import Index from "./pages/Index";
 import Courses from "./pages/Courses";
